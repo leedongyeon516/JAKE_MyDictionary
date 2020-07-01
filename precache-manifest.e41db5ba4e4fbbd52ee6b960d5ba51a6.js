24ec692acd6d2e804219169e3be92c17ba4a2e87
JAKE LEE <lee@JAKEs-MacBook-Pro.local>
2020-07-01 07:52:41 -0400

    Updates

---
+++ precache-manifest.e41db5ba4e4fbbd52ee6b960d5ba51a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47771cf1c08956426c7c6d605a71148d",
+    "url": "/JAKE_MyDictionary/index.html"
+  },
+  {
+    "revision": "9c76678ba26f94c824da",
+    "url": "/JAKE_MyDictionary/static/css/main.739ef80d.chunk.css"
+  },
+  {
+    "revision": "2484c57631c4df14796a",
+    "url": "/JAKE_MyDictionary/static/js/2.7a6f6964.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/JAKE_MyDictionary/static/js/2.7a6f6964.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c76678ba26f94c824da",
+    "url": "/JAKE_MyDictionary/static/js/main.5bbdab7d.chunk.js"
+  },
+  {
+    "revision": "9f35884c00d46e22c769",
+    "url": "/JAKE_MyDictionary/static/js/runtime-main.720fa6ca.js"
+  }
+]);
